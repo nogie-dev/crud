487594a590000c5fbbf4f3501a07cbce4d8d030b
nogy1op <layer7@kakao.com>
2022-04-08 19:49:25 +0900

    query@registerFuncAppended

--- db/query.js
+++ db/query.js
@@ -2,37 +2,60 @@ var db=require('./db')
 const conn=db.init()
 
 module.exports={
-    checkDupAccount:function(id){
-        var judge=0
-        conn.query("select id from users where id=?",[id],(err,res)=>{
-            if(err) console.log(err)
-            else console.log('query success')
-
-            //[RowDataPacket { id: 'hello' }] 배열 안에 딕셔너리가 존재함으로 다음과 같이 접근해야 한다.
-            try{
-                if(res[0]['id']===id){
-                    judge=1;
-                    //console.log("equal")
-                }
-            }catch{
-                console.log("error")
-            }
-        })
-        return judge;
-        //conn.end();
+    checkDupId:async function(id){
+        try{
+            let judge=false
+            let [rows,fields]=await conn.then((connection)=>connection.query("select id from users where id=?",[id]))
+            
+            if(rows[0]){judge=true} //rows[0]에 값이 있을 경우(중복) judge true 그렇지 않으면 false
+            return judge
+        }catch(error){
+            console.log(error)
+            //return {"status":"400","msg":"bad request"}
+        }
     },
 
-    userRegister:function(name,id,password){
+    checkDupName:async function(name){
         try{
-            await co
+            let judge=false
+            let [rows,fields]=await conn.then((connection)=>connection.query("select nickname from users where nickname=?",[name]))
+            
+            if(rows[0]){judge=true}
+            return judge
         }catch(error){
+            console.log(error)
+            //return {"status":"400","msg":"bad request"}
+        }
+    },
 
+    userRegister:async function(id,password,nickname){
+        try{
+            let checkId=false
+            this.checkDupId(id).then((result)=>checkId=result)
+
+            let checkName=false
+            this.checkDupName(nickname).then((result)=>checkName=result)
+
+            console.log(id,password,nickname)
+
+            if(checkId==false&&checkName==false){
+                await conn.then((connection)=>connection.execute("insert into users(nickname,id,password) values(?,?,?)",[nickname,id,password]))
+                return {"status":"200", "msg":"success registered user infomation"}
+            }else{
+                return {"status":'Duplicated', "msg":"there is a duplicated with either"}
+            }
+            //관리자 권한을 부여받아야 할 경우 mysql console로 직접 계정 추가해야함
+            
+        }catch(error){
+            console.log(error)
+            return {"status":"400","msg":"bad request"}
         }
     },
 
     getBoardList:async function(){
         try{
-            let [rows,field]=await conn.then((connection)=>connection.execute("select * from board"));
+            let [rows,field]=await conn.then((connection)=>connection.query("select * from board"));
+            console.log(rows)
             return rows
         }catch(error){
             console.log(error)
