5b920038382fee61ffcae3637e49ba9357426b1c
nogy1op <layer7@kakao.com>
2022-04-08 19:50:10 +0900

    index@junk code remove

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,14 @@ var express = require('express');
 const app = require('../app');
 var router = express.Router();
 
-const account=require('./account.js')
-const user=require('./users.js')
+// const account=require('./account.js')
+// const user=require('./users.js')
 
 //app.use(express.static(__dirname, "../public"));
 
 // default
 // router.use('/account',account);
 // router.use('/users',user);
-router.get('/',function(req,res){
-    res.render('test.ejs')
-})
 
 router.all('/',(req,res)=>{
     res.json({
